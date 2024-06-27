1ab88f2923b1594a70fc91eefb0b85e17f21bf81
Lewis Pyke <45111173+lewispyke@users.noreply.github.com>
2024-06-27 22:31:34 +0100

    WIP displaying questions
    Todo:
    * reset answer position

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,22 +5,122 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
     public TextAsset questionsCSV;
+    public Transform displayRoot;
+    public Transform welcomeDisplayPrefab;
+    public QuestionDisplay questionDisplayPrefab;
 
+    private Transform welcomeDisplay;
     private List<Question> questions;
+    private QuestionDisplay questionDisplay;
 
-    // Start is called before the first frame update
-    void Start()
+    private enum State
+    {
+        ShowingWelcome,
+        ShowingQuestion,
+        ShowingAnswer,
+        ShowingResult,
+    }
+
+    private int curQuestion = -1;
+    private State curState = State.ShowingWelcome;
+    private int questionAnswer = -1;
+
+    private float nextTransition = 5.0f;
+
+    private void Start()
     {
         questions = QuestionHelper.GetQuestionsFromCSV(questionsCSV);
         foreach (var q in questions)
         {
             Debug.Log(q.ToString());
         }
+
+        welcomeDisplay = Instantiate(welcomeDisplayPrefab, displayRoot);
+        welcomeDisplay.gameObject.SetActive(true);
+
+        questionDisplay = Instantiate(questionDisplayPrefab, displayRoot);
+        questionDisplay.gameObject.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        bool forceTransition = false;
+        if (curState == State.ShowingQuestion)
+        {
+            if (UserInput.Instance.NavigationInput.x > 0)
+            {
+                questionAnswer = 0;
+            }
+            else if (UserInput.Instance.NavigationInput.x < 0)
+            {
+                questionAnswer = 1;
+            }
+            else if (UserInput.Instance.NavigationInput.y > 0)
+            {
+                questionAnswer = 2;
+            }
+
+            if (questionAnswer != -1)
+            {
+                forceTransition = true;
+            }
+        }
+
+        if (forceTransition || nextTransition <= Time.time)
+        {
+            DoTransition();
+        }
+    }
+
+    private void DoTransition()
+    {
+        switch (curState)
+        {
+            case State.ShowingWelcome:
+            {
+                welcomeDisplay.gameObject.SetActive(false);
+                questionDisplay.gameObject.SetActive(true);
+
+                curState = State.ShowingQuestion;
+                questionAnswer = -1;
+                ++curQuestion;
+                questionDisplay.ShowQuestion(questions[curQuestion]);
+                nextTransition = float.MaxValue;
+            }
+            break;
+
+            case State.ShowingQuestion:
+            {
+                curState = State.ShowingAnswer;
+                questionDisplay.ShowAnswer(questionAnswer);
+                nextTransition = Time.time + 6;
+            }
+            break;
+
+            case State.ShowingAnswer:
+            {
+                if (curQuestion < questions.Count - 1)
+                {
+                    curState = State.ShowingQuestion;
+                    questionAnswer = -1;
+                    ++curQuestion;
+                    questionDisplay.ShowQuestion(questions[curQuestion]);
+                    nextTransition = float.MaxValue;
+                }
+                else
+                {
+                    curState = State.ShowingResult;
+                    questionDisplay.gameObject.SetActive(false);
+                    nextTransition = float.MaxValue;
+                }
+            }
+            break;
+
+            case State.ShowingResult:
+            {
+                // Do nothing...
+            }
+            break;
+        }
     }
 }
--- Assets/Scripts/QuestionDisplay.cs
+++ Assets/Scripts/QuestionDisplay.cs
@@ -9,12 +9,106 @@ public class QuestionDisplay : MonoBehaviour
     public TMPro.TextMeshProUGUI answer1Text;
     public TMPro.TextMeshProUGUI answer2Text;
 
+    public Vector2 givenAnswerPosition = new(0, 0);
 
-    public void Setup(Question question)
+    private TMPro.TextMeshProUGUI AnswerText
+    {
+        get {
+            return answer switch
+            {
+                0 => answer0Text,
+                1 => answer1Text,
+                2 => answer2Text,
+                _ => null,
+            };
+        }
+    }
+
+    public int answer = -1;
+
+    public void ShowQuestion(Question question)
     {
         questionText.text = question.questionText;
         answer0Text.text = question.answer0;
         answer1Text.text = question.answer1;
         answer2Text.text = question.answer2;
+
+        answer0Text.gameObject.SetActive(false);
+        answer0Text.gameObject.SetActive(false);
+        answer1Text.gameObject.SetActive(false);
+        answer2Text.gameObject.SetActive(false);
+
+        StartCoroutine(ShowQuestionCo());
+    }
+
+    public void ShowAnswer(int answer)
+    {
+        this.answer = answer;
+
+        StartCoroutine(ShowAnswerCo());
+    }
+
+    IEnumerator ShowQuestionCo()
+    {
+        questionText.gameObject.SetActive(true);
+
+        float alpha = 0f;
+        while (alpha < 1f)
+        {
+            alpha += 1.0f * Time.deltaTime;
+            questionText.alpha = alpha;
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(1.0f);
+
+        answer0Text.gameObject.SetActive(true);
+        alpha = 0f;
+        while (alpha < 1f)
+        {
+            alpha += 2.0f * Time.deltaTime;
+            answer0Text.alpha = alpha;
+            yield return null;
+        }
+
+        answer1Text.gameObject.SetActive(true);
+        alpha = 0f;
+        while (alpha < 1f)
+        {
+            alpha += 2.0f * Time.deltaTime;
+            answer1Text.alpha = alpha;
+            yield return null;
+        }
+
+        answer2Text.gameObject.SetActive(true);
+        alpha = 0f;
+        while (alpha < 1f)
+        {
+            alpha += 2.0f * Time.deltaTime;
+            answer2Text.alpha = alpha;
+            yield return null;
+        }
+    }
+
+    IEnumerator ShowAnswerCo()
+    {
+        Vector2 startPos = AnswerText.rectTransform.anchoredPosition;
+
+        float t = 0f;
+        while (t < 1f)
+        {
+            t += 1.0f * Time.deltaTime;
+            if (t > 1) t = 1;
+            float alpha = 1 - t;
+
+            questionText.alpha = alpha;
+            if (answer != 0) answer0Text.alpha = alpha;
+            if (answer != 1) answer1Text.alpha = alpha;
+            if (answer != 2) answer2Text.alpha = alpha;
+
+            AnswerText.rectTransform.anchoredPosition = Vector2.Lerp(startPos, givenAnswerPosition, t);
+
+            yield return null;
+        }
     }
 }
