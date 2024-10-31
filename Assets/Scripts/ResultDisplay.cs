593785701470bb7f6a610e59e3496cdc02e30d56
Lewis-Pyke <lewiswaitforit@gmail.com>
2024-10-31 08:26:42 +0000

    Fade in result

--- Assets/Scripts/ResultDisplay.cs
+++ Assets/Scripts/ResultDisplay.cs
@@ -3,6 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+public static class ImageHelper
+{
+    public static void SetAlpha(this Image image, float a)
+    {
+        a = Mathf.Clamp01(a);
+        image.color = new(image.color.r, image.color.g, image.color.b, a);
+    }
+}
+
 public class ResultDisplay : MonoBehaviour
 {
     public TMPro.TextMeshProUGUI nameText;
@@ -15,10 +24,32 @@ public class ResultDisplay : MonoBehaviour
         restartText.gameObject.SetActive(false);
         nameText.text = spirit.spiritName;
         image.sprite = spirit.image;
+
+        StartCoroutine(RevealResultCo());
     }
 
     public void ShowRestartText()
     {
         restartText.gameObject.SetActive(true);
+    }
+
+    private IEnumerator RevealResultCo()
+    {
+        nameText.alpha = 0.0f;
+        image.SetAlpha(0.0f);
+
+        yield return new WaitForSeconds(1);
+
+        float alpha = 0f;
+        while (alpha < 1f)
+        {
+            alpha += 1.0f * Time.deltaTime;
+            nameText.alpha = alpha;
+            image.SetAlpha(alpha);
+            yield return null;
+        }
+
+        nameText.alpha = 1.0f;
+        image.SetAlpha(1.0f);
     }
 }
